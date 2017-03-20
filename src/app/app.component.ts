3f92e8e05eb142f7fe52e493d122fb489e83607a
Willder Azevedo <willder@angulodigital.com.br>
2017-03-20 12:38:58 -0300

    page(config): pagina de configuração

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { HomePage } from '../pages/home/home';
+import { ConfigPage } from '../pages/config/config';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = ConfigPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ConfigPage } from '../pages/config/config';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    ConfigPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +14,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    ConfigPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
