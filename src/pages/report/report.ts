f0e5cd5d6bd12303bdc72c317515f5a50f426dda
Willder Azevedo <willderazevedo434@gmail.com>
2017-06-01 17:09:18 -0300

    feat(page-report): campos validados

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -13,14 +13,50 @@ import { ReportModalPage } from '../report-modal/report-modal';
 })
 export class ReportPage {
 
-  reportModal:Object = ReportModalPage;
-  out:boolean = false;
+  reportModal       = ReportModalPage;
+  minDate           = new Date();
+  maxDate           = new Date(new Date().setFullYear(new Date().getFullYear() + 5));
+  actualDate        = new Date();
+  tomorrowDate      = new Date(new Date().setDate(new Date().getDate() + 1));
+  hide              = false;
+  report_properties = {
+    beginDate: this.actualDate.toISOString(),
+    finalDate: this.tomorrowDate.toISOString(),
+    type: 1,
+    category: 1,
+    release_type: 1,
+    pay_form: 1,
+  };
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
   public global: GlobalService) {}
 
+  public changeBeginPeriod() {
+    let beginDate     = new Date(this.report_properties.beginDate);
+    let finalDate     = new Date(this.report_properties.finalDate);
+    let yesterdayDate = new Date(new Date(finalDate).setDate(finalDate.getDate() - 1));
+
+    if(finalDate > beginDate) {
+      return false;
+    }
+
+    this.report_properties.beginDate = yesterdayDate.toISOString();
+  }
+
+  public changeFinalPeriod() {
+    let beginDate    = new Date(this.report_properties.beginDate);
+    let finalDate    = new Date(this.report_properties.finalDate);
+    let tomorrowDate = new Date(new Date(beginDate).setDate(beginDate.getDate() + 1));
+
+    if(beginDate < finalDate) {
+      return false;
+    }
+
+    this.report_properties.finalDate = tomorrowDate.toISOString();
+  }
+
   public fieldPayForm(hide) {
-    this.out = hide;
+    this.hide = hide;
   }
 
 }
